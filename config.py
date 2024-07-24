6c44751a61a6d0563c9a8c10b270c7441e90ab1e
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2024-07-24 12:49:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,15 +39,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/code663/lover",
+    "https://github.com/WEREWOLF-DEMON/lover",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mr_editx")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+MfgrrItC5nY4ZGRl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/teamssssssdd")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/teamszsssa")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
